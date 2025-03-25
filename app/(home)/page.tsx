ba056b925a9c690055a12bf24fcb289980da6be4
baekmerve <baekmerve@gmail.com>
2025-03-26 03:20:21 +0900

    adding carousels

---
+++ app/(home)/page.tsx
@@ -0,0 +1,6 @@
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import data from '@/lib/data'
+
+export default function Page() {
+  return <HomeCarousel items={data.carousels} />
+}
