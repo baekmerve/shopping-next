54495ac2c7a9fb9421ed976b2efa28ce6b2cad34
baekmerve <baekmerve@gmail.com>
2025-03-26 03:21:51 +0900

    constants of local env

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Nextmall";
+export const APP_SLOGAN= process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, shop more";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Nextmall is a platform built with Next.js where you can buy and sell products online'
